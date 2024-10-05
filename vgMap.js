6bac4455975eacce68cf97048a3408a5841f90d2
sai-ramishetty <sairamishetty9@outlook.com>
2024-10-05 18:35:45 +1000

    updated pie chart

--- vgMap.js
+++ vgMap.js
@@ -1,4 +1,11 @@
 var vg_1 = "map.json"
+var vg_2 = "piechart.json"
+
 vegaEmbed("#cmap", vg_1).then(function(result) {
+    console.log("Chart rendered successfully.");
+  }).catch(console.error);
+
+
+  vegaEmbed("#pie", vg_2).then(function(result) {
     console.log("Chart rendered successfully.");
   }).catch(console.error);
